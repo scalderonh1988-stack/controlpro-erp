f53bbba7197f0ca4842940e460d210d39f19470a
Sebastian Calderon <scalderonh1988@gmail.com>
2026-08-16 13:32:58 -0400

    Cambio link de pago

--- app.py
+++ app.py
@@ -870,7 +870,7 @@ if st.sidebar.button("🚪 Cerrar Sesión", use_container_width=True):
 
 if not st.session_state.get("es_admin_dev", False):
     st.sidebar.write("") 
-    st.sidebar.link_button("💳 Renovar Licencia Mensual", "https://mpago.la/2aRRK8q", type="primary", use_container_width=True)
+    st.sidebar.link_button("💳 Renovar Licencia Mensual", "https://mpago.la/12ae7ej", type="primary", use_container_width=True)
 
 if not st.session_state.get("es_admin_dev", False):
     try:
